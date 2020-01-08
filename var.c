393401be5ab2e55b9cba5a4d2bf21231f5fb43b6
claudiogoncalves <claudiogoncalveslck@gmail.com>
2020-01-08 05:22:51 +0000

    update parcial menus

--- var.c
+++ var.c
@@ -1,15 +1,114 @@
-#include <stdio.h>
-#include <string.h>
 #include "structs.h"
+#include "stdlib.h"
 
-void menuCol(){
+void mainmenu()
+{
+    system("cls");
+    printf("**********************************\n");
+
+    printf("MAIN MENU\n\n");
+
+    printf("1- Colaboradores\n");
+
+    printf("2- Auditorias\n");
+
+    printf("3- Vulnerabilidades\n\n");
+
+    printf("**********************************\n");
+};
+void adicionarCol(){
 
 };
-
-void menuVuln(){
+void editarCol(){
 
 };
+void removerCol(){
 
-void menuAuditorias(){
+};
+void menuCol()
+{
+    int opcao;
+    do
+    {
+        system("cls");
+        printf("**********************************\n");
 
+        printf("MENU COLABORADORES\n\n");
+
+        printf("1- Adicionar\n");
+
+        printf("2- Editar\n");
+
+        printf("3- Remover\n\n");
+
+        printf("**********************************\n");
+
+        scanf("%d", opcao);
+        switch (opcao)
+        {
+        case 0:
+            break;
+        case 1:
+            adicionarCol();
+            break;
+        case 2:
+            editarCol();
+            break;
+        case 3:
+            removerCol();
+            break;
+        }
+    } while (opcao != 0);
+};
+
+void menuVuln()
+{
+    int opcao;
+    do
+    {
+        system("cls");
+        printf("**********************************\n");
+
+        printf("MENU VULNERABILIDADES\n\n");
+
+        printf("1- Adicionar\n");
+
+        printf("2- Editar\n");
+
+        printf("3- Remover\n\n");
+
+        printf("**********************************\n");
+
+        scanf("%d", opcao);
+        if (opcao = 0)
+        {
+            mainmenu();
+        }
+    } while (opcao != 0);
+};
+
+void menuAuditorias()
+{
+    int opcao;
+    do
+    {
+        system("cls");
+        printf("**********************************\n");
+
+        printf("MENU AUDITORIAS\n\n");
+
+        printf("1- Adicionar\n");
+
+        printf("2- Editar\n");
+
+        printf("3- Remover\n\n");
+
+        printf("**********************************\n");
+
+        scanf("%d", opcao);
+        if (opcao = 0)
+        {
+            mainmenu();
+        }
+    } while (opcao != 0);
 };
